7f3f19e211b011a47b818a63a8bb4e3512ef238e
Meedconf.id <ahmad'aqiltaqiyyuddin@smkitihsanulfikri.sch.id>
2026-02-11 09:27:37 +0700

    Update alarmpiket2.py
    CONFIG KELOMPOK

--- alarmpiket2.py
+++ alarmpiket2.py
@@ -19,3 +19,36 @@ def tentukan_kelompok(hari):
     else:
         return 3
 # (AZMI)
+
+def cek_piket_akbar():
+    tanggal_mulai = datetime.date(2025, 1, 6)  # bebas, patokan awal
+    hari_ini = datetime.date.today()
+    selisih_hari = (hari_ini - tanggal_mulai).days
+
+    if selisih_hari % 14 == 0:
+        return True
+    else:
+        return False
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+print("=== SISTEM BEL PIKET AKTIF ===")
+
+sudah_bunyi_pagi = False
+sudah_bunyi_sore = False
+tanggal_terakhir = ""
+
+while True:
+    sekarang = datetime.datetime.now()
+    jam = sekarang.strftime("%H:%M")
+    hari = sekarang.weekday()
+    tanggal = sekarang.strftime("%Y-%m-%d")
+
+    if tanggal != tanggal_terakhir:
+        sudah_bunyi_pagi = False
+        sudah_bunyi_sore = False
+        tanggal_terakhir = tanggal
+
+    kelompok = tentukan_kelompok(hari)
+#AQIL
