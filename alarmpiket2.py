917275ad122289c40f0659381d9a4a29950ecea2
muhammad azmi robbani <muhammadazmirobbani@smkitihsanulfikri.sch.id>
2026-02-11 08:55:42 +0700

    alarm2

--- alarmpiket2.py
+++ alarmpiket2.py
@@ -2,3 +2,20 @@ import time
 import datetime
 import os
 from playsound import playsound
+# (suta)
+def tentukan_kelompok(hari):
+    if hari == 0:
+        return 1
+    elif hari == 1:
+        return 2
+    elif hari == 2:
+        return 3
+    elif hari == 3:
+        return 4
+    elif hari == 4:
+        return 1
+    elif hari == 5:
+        return 2
+    else:
+        return 3
+# (AZMI)
